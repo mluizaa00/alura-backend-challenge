17611cf090fd4ab20f1f71c68b5fdb8175d67f46
BuildTools <taeckls@gmail.com>
2022-01-24 11:56:08 -0300

    first commit

---
+++ src/test/java/com/luizaprestes/alurabackendchallenge1/AluraBackendChallenge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizaprestes.alurabackendchallenge1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraBackendChallenge1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
