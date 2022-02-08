6d5d9bdfd0c634b9a413c7a4754d9ff5790d74f5
BuildTools <taeckls@gmail.com>
2022-02-08 17:10:56 -0300

    feat: profiles, DevSecurityAdapter

---
+++ src/main/java/com/luizaprestes/challenge/adapter/DevSecurityAdapter.java
@@ -0,0 +1,39 @@
+package com.luizaprestes.challenge.adapter;
+
+import com.luizaprestes.challenge.config.JwtEntryPoint;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Profile("dev")
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class DevSecurityAdapter extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private JwtEntryPoint entryPoint;
+
+  @Override
+  @SneakyThrows
+  protected void configure(final HttpSecurity security) {
+    security
+        .authorizeRequests()
+          .antMatchers("/").permitAll()
+          .and()
+              .exceptionHandling()
+              .authenticationEntryPoint(entryPoint)
+          .and()
+              .sessionManagement()
+              .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+          .and()
+        .logout().permitAll();
+  }
+
+}
--- src/main/java/com/luizaprestes/challenge/adapter/SecurityAdapter.java
+++ src/main/java/com/luizaprestes/challenge/adapter/SecurityAdapter.java
@@ -8,6 +8,7 @@ import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Profile("!dev")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
